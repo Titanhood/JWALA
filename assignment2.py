36d6551e3b4f5d7313939526b2cc68be693d7e59
Titanhood <ajay.srivastav0070@gmail.com>
2018-08-17 13:20:41 +0530

    Second Assignment

---
+++ assignment2.py
@@ -0,0 +1,24 @@
+#question_1
+print("dattebayo")
+#question_2
+a='acad'
+b='view'
+print(a+b)
+#question_3
+x=input()
+y=input()
+z=input()
+print(x,y,z)
+#question_4
+print("Let's get started")
+#question_5
+s='Acadview'
+course='Python'
+fees=5000
+print('%s' % s,course)
+print('%d' % fees)
+#question_6
+r=float(input('Enter Radius '))
+pi=3.14
+area=pi*r*r
+print(area)
