af3c094aad1afe5698155bddd602ee7fcbf69475
Titanhood <ajay.srivastav0070@gmail.com>
2018-08-28 09:55:13 +0530

    Exceptional Handling

---
+++ assignment7.py
@@ -0,0 +1,41 @@
+#question_1
+try:
+    a=3
+    if a<4:
+        a=a/(a-3)
+        print(a)
+except ZeroDivisionError:
+    print('Cannot divide by zero')
+
+#question_2
+try:
+    i=[1,2,3]
+    print(i[3])
+except:
+    print('Enter valid index')
+#question_3
+An Exception
+then NameError 
+ERROR IS NAMEERROR HI THERE AN EXCEPTION
+#question_4
+'''AbyB(2.0,3.0)- output will be c=-5'''
+'''AbyB(3.0,3.0)- output will be a/b result in 0'''
+#question_5
+'''Import error'''
+try:
+    import a
+except ImportError:
+    print('Import Invalid')
+'''value error'''
+try:
+    a=int()
+    print(a)
+except:
+    print('Enter a valid number')
+'''index error'''
+try:
+    i=[1,2,3]
+    print(i[3])
+except:
+    print('Enter valid index')
+
